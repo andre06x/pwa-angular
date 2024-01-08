97c57cca8ba5182779ebe204afd47482ab6a930a
André Silva <andre06x@gmail.com>
2024-01-08 17:00:50 -0300

    refatoran interface

--- src/app/models/Seguro.ts
+++ src/app/models/Seguro.ts
@@ -8,3 +8,12 @@ export class Seguro {
   sobrenomeProprietario: string | undefined;
   dataNascimentoProprietario: string | undefined;
 }
+
+export interface interfaceSeguro {
+  marcaCarro: MarcaCarro | null;
+  modeloCarro: string | null;
+  placaCarro: string | null;
+  nomeProprietario: string | null;
+  sobrenomeProprietario: string | null;
+  dataNascimentoProprietario: string | null;
+}
