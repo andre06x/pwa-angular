87393667ff69e44d54e6953e0297d5f3593bba08
André Silva <andre06x@gmail.com>
2024-01-08 17:01:41 -0300

    add service offline

---
+++ src/app/services/online-offline.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OnlineOfflineService {
+  private statusConexao$ = new Subject<boolean>();
+  constructor() {
+    window.addEventListener('online', () => {
+      this.atualizaStatusConexao();
+    });
+
+    window.addEventListener('offline', () => {
+      this.atualizaStatusConexao();
+    });
+  }
+
+  get isOnline(): boolean {
+    return window.navigator.onLine;
+  }
+
+  get statusConexao(): Observable<boolean> {
+    return this.statusConexao$.asObservable();
+  }
+
+  atualizaStatusConexao() {
+    console.log('chamou');
+    this.statusConexao$.next(this.isOnline);
+  }
+}
