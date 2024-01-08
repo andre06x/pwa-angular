7cf91c98ed689a99f5cc5feda0db1ab3798513c0
André Silva <andre06x@gmail.com>
2024-01-08 17:00:38 -0300

    refatorando c listar

--- src/app/components/listar-seguros/listar-seguros.component.ts
+++ src/app/components/listar-seguros/listar-seguros.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SeguroService } from '../../services/seguro.service';
+import { Observable } from 'rxjs';
+import { Seguro } from '../../models/Seguro';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listar-seguros',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
+  providers: [SeguroService],
   templateUrl: './listar-seguros.component.html',
-  styleUrl: './listar-seguros.component.css'
+  styleUrl: './listar-seguros.component.css',
 })
-export class ListarSegurosComponent {
+export class ListarSegurosComponent implements OnInit {
+  public seguros$: Observable<Seguro[]> | undefined;
 
+  constructor(private seguroService: SeguroService) {}
+
+  ngOnInit() {
+    this.seguros$ = this.seguroService.listar();
+  }
 }
